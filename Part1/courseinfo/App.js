87fd7e8a00c4d38e98306593be56a575dd531022
josemanuelmv89 <josemanuelmv89@gmail.com.com>
2022-12-12 23:16:51 -0400

    Texts Part

--- Part1/courseinfo/App.js
+++ Part1/courseinfo/App.js
@@ -1,33 +1,43 @@
-const Header = (props) => {
-    console.log(props)
-    return <h1>{props.course}</h1>;
+const Header = (header) => {
 
+  return <h1>{header.course}</h1>;
+
+};
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>
+     {props.name}   {props.exercises} 
+    </p>
+  );
+};
+
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
-  
-  const App = () => {
-    const course = {
-      name: "Half Stack application development",
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-        },
-      ],
-    };
-  
-    return (
+
+return (
       <div>
-        <Header  />
+      <Header course={course.name} />
+      <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+      <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
       </div>
-    );
-  };
-  
-  export default App;
+);
+};
