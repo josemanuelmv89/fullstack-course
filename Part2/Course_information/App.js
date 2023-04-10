79837acef53536bd5d4d6600d15731f160e823ae
josemanuelmv89 <josemanuelmv89@gmail.com.com>
2023-04-10 10:34:54 -0400

    SUMA

--- Part2/Course_information/App.js
+++ Part2/Course_information/App.js
@@ -1,106 +1,90 @@
-const Course = ({cours}) => {
+const Course = ({ cours }) => {
+  return (
+    <div>
+      <Header header={cours} />
+      <Content content={cours} />
+      <Suma suma={cours} />
+    </div>
+  );
+};
+
+const Header = ({ header }) => {
+  /*console.log('what is happening',header.id,header.name)*/
+  return <h1 key={header.id}>{header.name} </h1>;
+};
+const Content = ({ content }) => {
+  const varcontent = content.parts.map((c) => {
+    /* console.log('what is happening',c.id,c.name)*/
+    return <p key={c.id}> {c.name}</p>;
+  });
+  return <div>{varcontent}</div>;
+};
+const Suma = ({ suma }) => {
+  const varsuma = suma.parts.reduce((s, p) => {
+    /*console.log('what is happening',varsuma)*/
+    return s + p.exercises;
+  }, 0);
+  return (
+    <div>
+      <h2>The total is {varsuma}</h2>
+    </div>
+  );
+};
+const App = () => {
+  const course = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "node.js",
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+
+        {
+          name: "Middleware",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
-     <div>
-         <Header  header={cours}/> 
-         <Content content={cours}/>
-           
-           {/*<ul>Total of exercises {suma} </ul>*/}
-     </div>
-  )
-};
-
-const Header = ({header}) => {
-/*console.log('what is happening',cours.id,cours.name)*/
-return(
-  <h1 key={header.id} >{header.name} </h1>
- )
-};
-const Content = ({content}) => {
-
-return(
-  <div>
-    {content.parts.map((c)=> {
-           /* console.log('what is happening',c.id,c.name)*/
-              return <p key={c.id}> {c.name}</p>
-            })}
-      
-  </div>
-
-)
-};
-
-
-
-const App = () => {
-
-  const course = [
-  
-  {
-  
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-  
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-     ],
-  },
-  {
-    id: 2,
-    name: 'node.js',
-    parts: [
-      {
-        name: 'Routing',
-        exercises: 3,
-        id: 1,
-      },
-
-      {
-        name: 'Middleware',
-        exercises: 7,
-        id: 2,
-      },
-  ],
- },
-] 
-  
-       
- /*const suma = course.parts.reduce((s,p) =>
-  s + p.exercises,0 )
- console.log('what is happening',suma)*/
-   
-return (
-   <div>
-     {course.map((cours)=> {
-          
-      return <Course key={cours.id} cours={cours}/>
-       
+    <div>
+      {course.map((cours) => {
+        return <Course key={cours.id} cours={cours} />;
       })}
-
-   
-   </div>  
-  
+    </div>
   );
-
 };
-     
-export default App
+
+export default App;
