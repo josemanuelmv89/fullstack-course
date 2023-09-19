9a8626b0df87ec77394128b0402eef5e2e36126e
josemanuelmv89 <josemanuelmv89@gmail.com.com>
2023-09-19 10:35:23 -0400

    2.13:Data-for-Contries-Pass2

--- Part2/Countries/App.js
+++ Part2/Countries/App.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Show = ({ countries, newSearch }) => {
-  /*Filtro los paises y obtengo un arreglo de objetos*/
-  const varfilter = countries.filter((f) =>
-    f.name.common.toLocaleLowerCase().includes(newSearch.toLocaleLowerCase())
-  );
-  /*console.log("varfilter", varfilter);*/
+const Show = ({ varfilter, infocountry, selectcountry }) => {
   if (varfilter.length > 10) {
     return (
       <div>
@@ -14,32 +9,25 @@ const Show = ({ countries, newSearch }) => {
       </div>
     );
   } else if (varfilter.length === 1) {
-    return (
-      <div>
-        {/* Muestro el arreglo de objetos de tamaño 1*/}{" "}
-        {varfilter.map((coun) => {
-          return (
-            <div key={coun.capital}>
-              <h1>{coun.name.common}</h1>
-              <div>{`Capital: ${coun.capital}`}</div>
-              <h2>Spoken languages</h2>
-              <div>
-                {Object.values(coun.languages).map((p) => (
-                  <li key={p}>{p}</li>
-                ))}
-              </div>
-              <div>{`flag: ${coun.flags.svg}`}</div>
-            </div>
-          );
-        })}{" "}
-      </div>
-    );
+    return infocountry(varfilter);
   } else if (varfilter.length > 1 || varfilter.length <= 10) {
     return (
       <div>
         {/* Muestro el arreglo de objetos mayor que 1 o menor/igual que 10*/}{" "}
         {varfilter.map((coun) => {
-          return <ul key={coun.capital}> {coun.name.common}</ul>;
+          return (
+            <ul key={coun.capital}>
+              {" "}
+              {coun.name.common}{" "}
+              <button
+                onClick={() => {
+                  selectcountry(coun.name.common);
+                }}
+              >
+                Show
+              </button>
+            </ul>
+          );
         })}{" "}
       </div>
     );
@@ -70,16 +58,53 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     const serch = event.target.value;
-    console.log("what is value seach", serch);
+    console.log("newSearch", newSearch);
     setSearch(serch);
   };
+  /*Filtro los paises y obtengo un arreglo de objetos*/
+  const varfilter = countries.filter((f) =>
+    f.name.common.toLocaleLowerCase().includes(newSearch.toLocaleLowerCase())
+  );
 
-  /*Filtrado de paises por busqueda*/
+  /*Esta funcion muestra informacion de un pais en particular*/
+  const infocountry = (varfilter) => {
+    console.log("varfilter", varfilter);
+    return (
+      <div>
+        {/* Muestro el arreglo de objetos de tamaño 1*/}{" "}
+        {varfilter.map((coun) => {
+          return (
+            <div key={coun.capital}>
+              <h1>{coun.name.common}</h1>
+              <div>{`Capital: ${coun.capital}`}</div>
+              <h2>Spoken languages</h2>
+              <div>
+                {Object.values(coun.languages).map((p) => (
+                  <li key={p}>{p}</li>
+                ))}
+              </div>
+              <div>{`flag: ${coun.flags.svg}`}</div>
+            </div>
+          );
+        })}{" "}
+      </div>
+    );
+  };
+  /*Si el usuario preciona selectcountry se actualiza el estado de la variable newsearch*/
+  const selectcountry = (who) => {
+    const parametercountry = who;
+    setSearch(parametercountry);
+    console.log("newSearch", newSearch);
+  };
 
   return (
     <div>
       <Bar newSearch={newSearch} handleFilterChange={handleFilterChange} />
-      <Show countries={countries} newSearch={newSearch} />
+      <Show
+        varfilter={varfilter}
+        infocountry={infocountry}
+        selectcountry={selectcountry}
+      />
     </div>
   );
 };
