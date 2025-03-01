b8ccc4a4a0d283e6e2a28d71d937f449ae37c145
josemanuelmv89 <43415090+josemanuelmv89@users.noreply.github.com>
2025-03-01 18:31:12 -0400

    Delete ERROR directory

--- Part2/Countries/components/Bar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Bar = ({ newSearch, handleFilterChange }) => {/*Creacion del componente barra de entrada de nombres de paises*/
-  return (
-    <div>
-      Find countries: <input value={newSearch} onChange={handleFilterChange} />
-    </div>
-  );
-};
-export default Bar;
--- Part2/Phonebook/components/Filter.js
+++
@@ -1,8 +0,0 @@
-const Filter = ({newSearch,handleFilterChange}) => {
-  return (
-    <div>
-      Filter for name: <input value={newSearch} onChange={handleFilterChange} />
-    </div>
-  );
-};
-export default Filter;
