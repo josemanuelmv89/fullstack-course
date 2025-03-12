11c84dca9ead06f3d34ae2cc7481958bac634fc7
josemanuelmv89 <josemanuelmv89@gmail.com.com>
2025-03-12 14:33:11 -0430

    3.5:Backend-Phonebook-Pass 5

--- Part3/Backend_Phonebook/index.js
+++ Part3/Backend_Phonebook/index.js
@@ -54,6 +54,32 @@ let persons = [
     response.status(204).end()
   })
   
+  const generateId = () => {
+    const maxId = Math.floor(Math.random() * 100)
+    return maxId
+  }
+  
+  app.post("/api/persons", (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({
+        error: "content missing",
+      })
+    }
+  
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: Boolean(body.number) || false,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+    
+  })
+  
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
